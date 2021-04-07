d3fc953b2e0dfd43f249de67f0dc2733b8669beb
StoneJaeger <arthoskar@gmail.com>
2021-04-06 21:40:36 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FizzbuzzComponent } from './fizzbuzz/fizzbuzz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FizzbuzzComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/fizzbuzz/fizzbuzz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fizzbuzz',
+  templateUrl: './fizzbuzz.component.html',
+  styleUrls: ['./fizzbuzz.component.scss']
+})
+export class FizzbuzzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
