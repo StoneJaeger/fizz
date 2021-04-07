89fb4c19d2837f119611bf68deba34f7570524b4
StoneJaeger <arthoskar@gmail.com>
2021-04-06 22:06:24 -0400

    Modifiying app.component to show the title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fizzbuzz'`, () => {
+  it(`should have as title 'Fizzbuzz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fizzbuzz');
+    expect(app.title).toEqual('Fizzbuzz');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fizzbuzz app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Fizzbuzz');
   });
 });
+ 
